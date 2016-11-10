52d3d094ebeb832b9e82717122d0fcd3462a564b
ValentinLipatov <valentin-ltd@mail.ru>
2016-11-10 22:56:34 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,10 +1,15 @@
 <?php
-$dbconn = pg_connect("host=ec2-54-75-228-85.eu-west-1.compute.amazonaws.com dbname=ddrgbsg3qokpc2 user=kbpivkrmvdkauu password=0p_EhxRACs9Q2b96sZ5Fs3zK_m")or die('Could not connect: ' . pg_last_error());
+$dbconn = pg_connect("
+	host     = ec2-54-75-228-85.eu-west-1.compute.amazonaws.com
+	dbname   = ddrgbsg3qokpc2
+	user     = kbpivkrmvdkauu
+	password = 0p_EhxRACs9Q2b96sZ5Fs3zK_m
+")or die('Could not connect: ' . pg_last_error());
 if($_GET["type"] == "get")
 {
 	$query = 'SELECT * FROM users';
 	$result = pg_query($query) or die(pg_last_error());
-    	$line = pg_fetch_array($result, null, PGSQL_ASSOC)
+    	
 	echo "name";
 }
 pg_free_result($result);
