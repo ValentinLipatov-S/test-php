53453f2d8fa3290e4608992a6a5fbc68b75257d8
ValentinLipatov <valentin-ltd@mail.ru>
2016-11-10 18:11:59 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -3,12 +3,13 @@ $dbconn = pg_connect("host=ec2-54-75-233-142.eu-west-1.compute.amazonaws.com dbn
 if($_GET["type"] == "create")
 {
 	$query = 'CREATE TABLE users (name TEXT)';
-	$result = pg_query($query) or die('ќшибка запроса: ' . pg_last_error());
+	$result = pg_query($query) or die('error: ' . pg_last_error());
+	echo "execute";
 }
 if($_GET["type"] == "get")
 {
 	$query = 'SELECT * FROM users';
-	$result = pg_query($query) or die('ќшибка запроса: ' . pg_last_error());
+	$result = pg_query($query) or die('error: ' . pg_last_error());
 	echo "<table>\n";
 	while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
 		echo "\t<tr>\n";
@@ -22,7 +23,8 @@ if($_GET["type"] == "get")
 if($_GET["type"] == "set")
 {
 	$query = "INSERT INTO users ('name') VALUES ('$_GET[name]')";
-	$result = pg_query($query) or die('ќшибка запроса: ' . pg_last_error());
+	$result = pg_query($query) or die('error: ' . pg_last_error());
+	echo "execute";
 }
 pg_free_result($result);
 pg_close($dbconn);
