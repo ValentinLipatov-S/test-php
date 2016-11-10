8817205d4715f82e84cdb37a5eb8ba0ac216ea1b
ValentinLipatov <valentin-ltd@mail.ru>
2016-11-10 19:38:58 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,14 +1,15 @@
 <?php
-$dbconn = pg_connect("host=ec2-54-75-233-142.eu-west-1.compute.amazonaws.com dbname=d49ap8m67d6okt user=gjowfbdepubgzi password=EMRp_nMC4f46cvcYTCm3Lr_jcs")or die('Could not connect: ' . pg_last_error());
+$dbconn = pg_connect("host=ec2-54-75-228-85.eu-west-1.compute.amazonaws.com dbname=ddrgbsg3qokpc2 user=kbpivkrmvdkauu password=0p_EhxRACs9Q2b96sZ5Fs3zK_m")or die('Could not connect: ' . pg_last_error());
 if($_GET["type"] == "create")
 {
-	$query = 'CREATE TABLE users (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, firstname TEXT, secondname TEXT, login TEXT, password TEXT)';
-	$result = pg_query($query) or die(pg_last_error());
+	$query = 'CREATE TABLE users (name TEXT)';
+	$result = pg_query($query) or die('error: ' . pg_last_error());
+	echo "execute";
 }
 if($_GET["type"] == "get")
 {
 	$query = 'SELECT * FROM users';
-	$result = pg_query($query) or die(pg_last_error());
+	$result = pg_query($query) or die('error: ' . pg_last_error());
 	echo "<table>\n";
 	while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
 		echo "\t<tr>\n";
@@ -21,61 +22,10 @@ if($_GET["type"] == "get")
 }
 if($_GET["type"] == "set")
 {
-	$query = "INSERT INTO users ('name') VALUES ('$_GET[name]')";
-	$result = pg_query($query) or die(pg_last_error());
+	$query = "INSERT INTO users VALUES('$_GET[name]')";
+	$result = pg_query($query) or die('error: ' . pg_last_error());
+	echo "execute";
 }
-
-
-
-
-
-function Autorization($login, $password)
-{
-	$query = "SELECT * FROM users WHERE login = '$login' LIMIT 1";
-	$result = pg_query($query) or die(pg_last_error());
-	$data = mysqli_fetch_assoc($result);
-	if($data['password'] === $password)	return $data['id'];
-	else return "error";
-}
-
-
-
-
-
-
-if($_GET["type"] == "autorization" and isset($_GET['login']) and isset($_GET['password']))
-{
-	$aut = Autorization($_GET['login'], $_GET['password'])
-	if($aut === "error")
-	{
-		echo "error<-msg->login or password";
-	}
-	else 
-	{
-		echo "sucess<-msg->";
-	}
-	
-}
-if($_GET["type"] == "registration")
-{
-	$query = "INSERT INTO users VALUES ('', '$_GET[firstname]', '$_GET[secondname]', '$_GET[login]', '$_GET[password]')";
-	$result = pg_query($query) or die(pg_last_error());
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 pg_free_result($result);
 pg_close($dbconn);
 ?>
