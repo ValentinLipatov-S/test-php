c58a305ed3f491da994927e13cfe390a375a0701
ValentinLipatov <valentin-ltd@mail.ru>
2016-11-11 02:08:08 +0300

    Update login.js

--- web/scripts/login.js
+++ web/scripts/login.js
@@ -1,9 +1,12 @@
 $(document).ready(function()
 {	
+	var login, password;
 	$('#Login').click(function()
 	{			
 		$("div[id='message']").slideUp(300);
 		$("div[id='download']").slideDown(300);
+		var user_login_now = $("#Login_Login").val();
+		var user_password_now = $("#Login_Password").val();
 		$.ajax
 		({
 			type: "GET",
@@ -11,8 +14,8 @@ $(document).ready(function()
 			data: 
 			{
 				comand:   	   "autorization", 
-				user_login:    $("#Login_Login").val(), 
-				user_password: $("#Login_Password").val()
+				user_login:    user_login_now, 
+				user_password: user_password_now
 			},
 			success: function(msg)
 			{
@@ -20,16 +23,21 @@ $(document).ready(function()
 				$("#p_message").html(arr[0] + ":<br>" + arr[1]);
 				if(arr[0] == "SUCCESS")
 				{	
+					login = user_login_now;
+					password = user_password_now;
 					$("div[id='Login_Registration']").slideUp(300);
 					$("input[id='Exit']").slideDown(300);
-					$("div[id='ChatRooms']").slideDown(300);	
+					AddChatRooms();					
 				}
-				$("div[id='message']").slideDown(300);
-				$("div[id='download']").slideUp(300);
-				setTimeout(function() 
+				else
 				{
-					$("div[id='message']").slideUp(300);	
-				}, 3000);
+					$("div[id='message']").slideDown(300);
+					$("div[id='download']").slideUp(300);
+					setTimeout(function() 
+					{
+						$("div[id='message']").slideUp(300);	
+					}, 3000);
+				}
 			}					 
 		});			
 	});
@@ -63,4 +71,36 @@ $(document).ready(function()
 			}					 
 		});			
 	});
+	
+	
+	
+	function AddChatRooms()
+	{
+		$("div[id='ChatRooms']").slideDown(300);
+		$("div[id='download']").slideDown(300);
+		$("#ChatRooms_List").empty();
+		$.ajax
+		({
+			type: "GET",
+			url: "server.php",
+			data: 
+			{
+				comand: 'chatrooms', 
+				user_login: login,
+				user_password: password
+			},
+			success: function(msg)
+			{
+				var arr = msg.split('<-name->');
+				arr.forEach(function(item, i, arr) 
+				{
+					var arr_2 = item.split('<-id->');
+				    $('<input id = "' + arr_2[0] + '" class = "ChatRoomsButton" type = "submit" value = "' + arr_2[1] + '" style = "background: #e3f1ed; display: none;" />').appendTo($("#ChatRooms_Spisok"));
+				    $("input[class = 'ChatRoomsButton']").slideDown(500);	
+				});	
+				$("div[id='download']").slideUp(300);
+			}
+		});		
+		
+	}
 });
