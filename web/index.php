05698181ad06685413998ffbc57897ed4af39b7f
ValentinLipatov <valentin-ltd@mail.ru>
2016-11-10 18:10:27 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,25 +1,29 @@
 <?php
-
-require('../vendor/autoload.php');
-
-$app = new Silex\Application();
-$app['debug'] = true;
-
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
-
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
-
-$app->run();
+$dbconn = pg_connect("host=ec2-54-75-233-142.eu-west-1.compute.amazonaws.com dbname=d49ap8m67d6okt user=gjowfbdepubgzi password=EMRp_nMC4f46cvcYTCm3Lr_jcs")or die('Could not connect: ' . pg_last_error());
+if($_GET["type"] == "create")
+{
+	$query = 'CREATE TABLE users (name TEXT)';
+	$result = pg_query($query) or die('ќшибка запроса: ' . pg_last_error());
+}
+if($_GET["type"] == "get")
+{
+	$query = 'SELECT * FROM users';
+	$result = pg_query($query) or die('ќшибка запроса: ' . pg_last_error());
+	echo "<table>\n";
+	while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+		echo "\t<tr>\n";
+		foreach ($line as $col_value) {
+			echo "\t\t<td>$col_value</td>\n";
+		}
+		echo "\t</tr>\n";
+	}
+	echo "</table>\n";
+}
+if($_GET["type"] == "set")
+{
+	$query = "INSERT INTO users ('name') VALUES ('$_GET[name]')";
+	$result = pg_query($query) or die('ќшибка запроса: ' . pg_last_error());
+}
+pg_free_result($result);
+pg_close($dbconn);
+?>
