caca185788fb289045db6a69d40db9380c9b060e
ValentinLipatov <valentin-ltd@mail.ru>
2016-11-14 02:40:46 +0300

    Update login.js

--- web/scripts/login.js
+++ web/scripts/login.js
@@ -452,7 +452,7 @@ $(document).ready(function()
 		}); 
 			
 	});
-	//setInterval(function(){if(flag == true && work == false){Message_Timer();}},100);
+	setInterval(function(){if(flag == true && work == false){$('#Load').click();}},100);
 
 	var Stop = -1;
 	var Start = -1;
